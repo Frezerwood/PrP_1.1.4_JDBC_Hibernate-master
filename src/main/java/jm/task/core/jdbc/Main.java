c3fd04b7007fa00549f19f740909e30ca50424f1
Aleksandr Lomakin <samarazoyl@gmail.com>
2023-09-23 02:38:57 +0400

    Final correction

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,20 +8,16 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-
         UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Sam","lo",(byte) 23);
+        userService.saveUser("Harry","Ki",(byte) 44);
+        userService.saveUser("Ann","Grant",(byte) 33);
+        userService.saveUser("Tony","F",(byte) 54);
 
-        //userService.getAllUsers();
-         userService.dropUsersTable();
-//        userService.createUsersTable();
-//         userService.saveUser("Sam","lo",(byte) 23);
-//        userService.saveUser("Harry","Ki",(byte) 44);
-//        userService.saveUser("Ann","Grant",(byte) 33);
-//        userService.saveUser("Tony","F",(byte) 54);
+        userService.removeUserById(2);
 
-       // userService.removeUserById(2);
-//
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,6 +8,9 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
@@ -38,5 +41,20 @@ public class Util {
             }
         }
         return sessionFactory;
+    }
+
+    public static Connection getConnection() {
+        // реализуйте настройку соеденения с БД
+        String PASS = "8RCDNjQUwpGg";
+        String USER = "root";
+        String URL = "jdbc:mysql://localhost:3306/kata";
+
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
     }
 }
